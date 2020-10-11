97bc30c55f6994d597c4f8afe551c3c33a4f6782
jjkotni <kjjswaroop@gmail.com>
2020-10-11 10:53:15 +0530

    Setup script modifications

--- invoker.start.bash
+++ invoker.start.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Assuming current working directory is openwhisk home directory
 # Step1: Clean up current invoker docker
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 cd ansible
 ansible-playbook invoker.yml -e mode=clean
 cd ..
--- setup.bash
+++ setup.bash
@@ -1,27 +1,29 @@
 #!/bin/bash
 export OPENWHISK_TMP_DIR=/home/kjj/tmp
 export WSK_CONFIG_FILE=/home/kjj/openwhisk/.wskprops
+export JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 # Using virtualenv, python2.7 change ansible/environment/local/group_vars/all and add
 # "ansible_python_interpreter: <Path to python interpreter>"
 # Move to openwhisk source folder
 # docker-compose install specific version [1.24.1 stable for now]
 # npm, nodejs must be installed
+
 #Free port for couchdb
-sudo kill -9 $(sudo lsof -t -i:4369)
+# sudo kill -9 $(sudo lsof -t -i:4369)
 
 # Setup stuff, create db_local.ini
 cd ansible
 ansible-playbook setup.yml
 # Make sure that db_host: 172.17.0.1 in db_local.ini
 cd ..
-./gradlew distDocker
+./gradlew distDocker -PdockerImagePrefix=openwhisk -PdockerImageTag=nightly
 cd ansible
 # Create couchdb instance and initialize it
 ansible-playbook couchdb.yml
 ansible-playbook initdb.yml
 ansible-playbook wipe.yml
 # After this initialize openwhisk
-ansible-playbook openwhisk.yml
+ansible-playbook openwhisk.yml -e docker_image_prefix=openwhisk -e docker_image_tag=nightly -e pid_mode=host
 # To install catalog of useful packages
 # ansible-playbook postdeploy.yml
 # To use API Gateway
